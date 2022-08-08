e987596f88a71b4f63e7aec1de97d018223e62f3
cybermin <sillamin@naver.com>
2022-08-08 13:24:19 +0900

    df

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ import pandas
 st.title('부산 RFID음식물 쓰레기 분석')
 st.header('1. 데이터 불러오기')
 st.subheader('1-1. 판다스로 csv 파일 읽기')
+
+# 데이터 프레임 불러오기
+df = pd.read_csv('./부산RFID음식물쓰레기.csv')
+st.dataframe(df)
