db486a82e2bb5c9b090ed01b6dd7e94066ff376a
cybermin <sillamin@naver.com>
2022-08-08 13:38:41 +0900

    df3

--- app.py
+++ app.py
@@ -16,6 +16,6 @@ max_num1 = f"{df['배출량'].max():,}"
 max_area1 = f"{df[df['배출량'] == df['배출량'].max()]['시군구명']}"
  
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2 = st.columns(2)
 col1.metric('배출량 최대값 :', max_num1)
 col2.metric('배출량 최대 지역 :', max_area1)
