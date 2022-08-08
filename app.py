4448ef6f524846b32929bc9792083621ac0d7d32
cybermin <sillamin@naver.com>
2022-08-08 13:41:16 +0900

    sf

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.dataframe(df)
 max_num1 = f"{df['배출량'].max():,}"
 max_area1 = f"{df[df['배출량'] == df['배출량'].max()]['시군구명']}"
  
-st.write(df[df['배출량'] == df['배출량'].max()]['시군구명'])
+st.write(str(df[df['배출량'] == df['배출량'].max()]['시군구명']))
 
 col1, col2 = st.columns(2)
 col1.metric('배출량 최대값 :', max_num1)
