0d936c0ceb4b7dad6db5623d99143d4dae496cba
cybermin <sillamin@naver.com>
2022-08-09 20:57:35 +0900

    s1

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ st.dataframe(df)
 
 # 지표 표시
 max_num1 = f"{df['배출량'].max():,}"
-max_area1 = f"{df[df['배출량'] == df['배출량'].max()]['시군구명']}"
+max_area1 = f"{df[df['배출량'] == df['배출량'].max()].index}"
  
-st.write(str(df[df['배출량'] == df['배출량'].max()]['시군구명']))
+ 
 
 col1, col2 = st.columns(2)
 col1.metric('배출량 최대값 :', max_num1)
-#col2.metric('배출량 최대 지역 :', max_area1)
+col2.metric('배출량 최대 지역 :', max_area1)
