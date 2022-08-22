5cf8a321700266c4a986615d36a3ab596392ad6d
cybermin <sillamin@naver.com>
2022-08-22 17:13:28 +0900

    app

--- app.py
+++ app.py
@@ -20,4 +20,9 @@ df = pd.read_csv('기숙사수용현황.csv', encoding='cp949')
 
 st.header('지표 표시')
 #st.matric(label='전국대학수', value=len(df['학교'].unique()))
-st.write(len(df['학교'].unique()))
+#st.write(len(df['학교'].unique()))
+
+col1, col2, col3 = st.columns(3)
+col1.metric("전국대학수", "70 °F", "1.2 °F")
+col2.metric("국공립학교수", "9 mph", "-8%")
+col3.metric("사립학교수", "86%", "4%")
