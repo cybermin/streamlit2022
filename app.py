4d1a55806b9065a86d153d203434f3351b4964d3
cybermin <sillamin@naver.com>
2022-08-22 17:13:52 +0900

    app

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ st.header('지표 표시')
 #st.write(len(df['학교'].unique()))
 
 col1, col2, col3 = st.columns(3)
-col1.metric("전국대학수", "70 °F", "1.2 °F")
+col1.metric("전국대학수", len(df['학교'].unique()), "1.2 °F")
 col2.metric("국공립학교수", "9 mph", "-8%")
 col3.metric("사립학교수", "86%", "4%")
