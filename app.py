4d4d45d61bd6f66201b0d9258faa8e2e860693a9
cybermin <sillamin@naver.com>
2022-08-22 17:04:17 +0900

    app

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ import pandas as pd
 # 제목
 st.title('대학 기숙사 분석')
 st.header('1. 데이터 불러오기')
-st.subheader('1-1. 판다스로 csv 파일 읽기')
 
 # 데이터 프레임 불러오기
 df = pd.read_csv('기숙사수용현황.csv', encoding='cp949')
+
+st.subheader('동적 데이터 프레임 표시')
 st.dataframe(df)
+
+st.subheader('정적 데이터 프레임 표시')
+st.table(df)
 
