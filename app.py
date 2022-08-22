286200be1adcb4f1428583f3df4aacf320757395
cybermin <sillamin@naver.com>
2022-08-22 17:02:57 +0900

    app

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ st.header('1. 데이터 불러오기')
 st.subheader('1-1. 판다스로 csv 파일 읽기')
 
 # 데이터 프레임 불러오기
-df = pd.read_csv('기숙사수용현황.csv')
+df = pd.read_csv('기숙사수용현황.csv', encoding='cp949')
 st.dataframe(df)
 
